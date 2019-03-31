d31b898fe959348411b9b60ae4fe6370b5e52199
内田大輔 <one.two.deculture@docomo.ne.jp>
2019-03-31 21:06:16 +0900

    Initial Commit

---
+++ FlashCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashCard
+//
+//  Created by sue on 2019/03/31.
+//  Copyright © 2019 sue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
